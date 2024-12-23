ac49a33575f75ce4772d25ddcc2b6647d0509c00
kudryashhh <anya.kudryashova.05@mail.ru>
2024-12-23 15:58:46 +0300

    adaptive

--- src/styleguide.js
+++ src/styleguide.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     var navigation = document.querySelector(".navigation");
+    var up_button = document.querySelector(".up_button");
     var top = navigation.offsetTop;
 
     window.addEventListener("scroll", function () {
@@ -7,10 +8,12 @@ document.addEventListener("DOMContentLoaded", function () {
             navigation.style.position = "fixed";
             navigation.style.top = "-33%"; 
             navigation.style.zIndex = "10"; 
+            up_button.style.display = 'block';
         } else {
             navigation.style.position = ""; 
             navigation.style.top = ""; 
             navigation.style.zIndex = "";
+            up_button.style.display = 'none';
         }
     });
 });
