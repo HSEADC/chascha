439249bc754b42f1874ad0fb0d6af946d628fe79
Adelina <adrikadyrova@edu.hse.ru>
2024-12-24 09:15:27 +0300

    fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import './index.css'
 import jpg from './images/image.jpg'
 
+import './fonts/ofont.ru_Arial Narrow.ttf';
+import './fonts/arialnarrow_bold.ttf';
+import './fonts/arialnarrow_italic.ttf';
+import './fonts/arialnarrow_bolditalic.ttf';
+
 // document.addEventListener('DOMContentLoaded', () => {
 //   const image = new Image()
 //   image.src = jpg
